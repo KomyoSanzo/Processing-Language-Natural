4cb6f2335a078b5d4e9f1dcbd946d3b121be216e
Katie Chang <katiechang@Katies-MacBook.local>
2016-04-19 13:33:10 -0400

    writeup

--- Homework 6/vtag.py
+++ Homework 6/vtag.py
@@ -1,12 +1,11 @@
 '''
-Problem 2-3
+Problem 4, 5
 @author: Willis Wang and Hamster
 '''
 
 import sys
 import math
-from numpy.core.test_rational import denominator
-
+from collections import namedtuple
 
 def test(file_name):
     test_data = file(file_name, 'r')
@@ -16,9 +15,14 @@ def test(file_name):
     words = [None]*len(data)
     tags = [None]*len(data)
     probabilities = dict()
+    backpointers = dict()
+
+    mus = dict()
+    mus["###/0"] = 0.0
     
     words[0] = "###"
-    
+
+
     #VITERBI'S SHIT
     for i in range(1, len(data)):
         line = data[i].rstrip("\n")
@@ -43,8 +47,8 @@ def test(file_name):
     
     for i in range(len(data)-1, 0, -1):
         t[i-1] = backpointers[t[i] + "/" + str(i)]
-        totalProb += probabilities[t[i] + "/" + str(i)]
-    
+        totalProb += probabilities[t[i] + "/" + str(i)]      
+
     knownCorrect = 0.0
     knownTotal = 0.0
     
@@ -69,27 +73,95 @@ def test(file_name):
     print "Perplexity per Viterbi-tagged test word: %.3f" % math.exp(-totalProb/(len(words)-1))
                  
         
-
-def prob_tt(t_1, t_2):
-    if t_1 + "/" + t_2 in tt_count:
-        numerator = tt_count[t_1+ "/" + t_2]+1
-    else:
-        numerator = 1
-    denominator = tag_count[t_2] + len(all_training_tags)+1.0
+def forward_backward(file_name):
+    test_data = file(file_name, 'r')
+    data = test_data.readlines()
     
+    global words 
+    words = [None]*len(data)
+    tags = [None]*len(data)
+    
+    
+    words[0] = "###"
+    
+    alpha = dict()
+    alpha["###/0"] = 0
+    for i in range(1, len(data)):
+        line = data[i].rstrip("\n")
+        line = line.split("/")
+        words[i] = line[0]
+        tags[i] = line[1]
+        
+        for t_i in tag_set.get(words[i], all_training_tags):
+            for t_i_1 in tag_set.get(words[i-1], all_training_tags):
+                p = prob_tt(t_i, t_i_1) + prob_wt(words[i], t_i)
+                alpha[t_i+"/"+str(i)] = logsumexp(alpha.get(t_i+"/"+str(i), float('-inf')),
+                                                  alpha.get(t_i_1+"/"+str(i-1), float('-inf'))+p)
+    S = alpha["###"+"/"+str(len(words)-1)]
+    
+    BestTag = namedtuple("BestTag", "tag probability")
+    probabilities = [BestTag(None, float('-inf'))]*len(words)
+    
+    beta = dict()
+    beta["###"+"/"+str(len(words)-1)] = 0
+    for i in range(len(data)-1, -1, -1):
+        for t_i in tag_set.get(words[i], all_training_tags):
+            if probabilities[i].probability < alpha.get(t_i+"/"+str(i),float('-inf')) + beta.get(t_i+"/"+str(i),float('-inf')) - S:
+                probabilities[i] = BestTag(t_i, alpha.get(t_i+"/"+str(i),float('-inf')) + beta.get(t_i+"/"+str(i),float('-inf')) - S)
+            for t_i_1 in tag_set.get(words[i-1], all_training_tags):
+                p = prob_tt(t_i, t_i_1) + prob_wt(words[i], t_i)
+                beta[t_i_1+"/"+str(i-1)] = logsumexp(beta.get(t_i_1+"/"+str(i-1),float('-inf')),
+                                                     beta.get(t_i+"/"+str(i), float('-inf'))+p)
+    
+    knownCorrect = 0.0
+    knownTotal = 0.0
+                
+    newCorrect = 0.0
+    newTotal = 0.0
+    for i in range(len(words)):
+        if words[i] == "###":
+            continue
+        if tags[i] == probabilities[i].tag:
+            if words[i] in vocab:
+                knownCorrect += 1
+            else:
+                newCorrect += 1
+        if words[i] in vocab:
+            knownTotal += 1
+        else:   
+            newTotal += 1
+    print "Tagging accuracy (posterior decoding): %.2f%% (known: %.2f%% novel: %.2f%%)" % (100 * (newCorrect + knownCorrect) / (newTotal + knownTotal),
+                                                                                         100 * knownCorrect / knownTotal, 0 if newTotal == 0 else 100 * newCorrect / newTotal)
+
+
+def logsumexp(x, y):
+    if y <= x:
+        return x + math.log(1 + math.exp(y-x))
+    else:
+        return y + math.log(1 + math.exp(x-y))
+    
+def prob_tt(t_1, t_2):
+    lbda = 1.0 + tt_singleton[t_2]
+    numerator = tt_count.get(t_1+"/"+t_2,0) + lbda*prob_tt_backoff(t_1)
+    denominator = tag_count.get(t_2, 0) + lbda
     return math.log(float(numerator)/float(denominator))
+
+def prob_tt_backoff(t):
+    return tag_count[t]/(len(words)-1.0)
+
 
 def prob_wt(w, t):
+    
     if w == "###" and t == "###":
         return 0
-    
-    if w + "/" + t in tw_count:
-        numerator = tw_count[w + "/" + t] + 1
-    else:
-        numerator = 1
-    denominator = tag_count[t] + len(vocab)
+    lbda = 1 + tw_singleton[t]
+    numerator = tw_count.get(w+"/"+t,0) + lbda*prob_wt_backoff(w)
+    denominator = tag_count.get(t,0) + lbda
     
     return math.log(float(numerator)/float(denominator))
+
+def prob_wt_backoff(w):
+    return (vocab.get(w,0) + 1.0)/(len(vocab) + len(words) + 1.0)
 
 
 def train(file_name):
@@ -125,14 +197,30 @@ def train(file_name):
         else:
             tw_count[hashed_tw] += 1
         
+        #CHECK SINGLETONS FOR TW
+        if line[1] not in tw_singleton:
+            tw_singleton[line[1]] = 0
+            
+        if tw_count[hashed_tw] == 1:
+            tw_singleton[line[1]] += 1
+        elif tw_count[hashed_tw] == 2:
+            tw_singleton[line[1]] -= 1
         
         #CHECK IF EXISTS IN TT_COUNT AND INCREMENT
         if last_line != None:
-           hashed_tt = line[1] + "/" + last_line
-           if hashed_tt not in tt_count:
-               tt_count[hashed_tt] = 1
-           else:
-               tt_count[hashed_tt] += 1
+            hashed_tt = line[1] + "/" + last_line
+            if hashed_tt not in tt_count:
+                tt_count[hashed_tt] = 1
+            else:
+                tt_count[hashed_tt] += 1
+                
+            #CHECK SINGLETONS FOR TT
+            if last_line not in tt_singleton:
+                tt_singleton[last_line] = 0
+            if tt_count[hashed_tt] == 1:
+                tt_singleton[last_line] += 1
+            elif tt_count[hashed_tt] == 2:
+                tt_singleton[last_line] -= 1
         
         if line[1] != "###":
             all_training_tags.add(line[1])
@@ -144,15 +232,16 @@ def train(file_name):
 
 all_training_tags = set([])
 
+tw_singleton = dict()
+tt_singleton = dict()
+
 tt_count = dict()
 tw_count = dict()
+
+
 vocab = dict()
 tag_count = dict()
 tag_set = dict()
-backpointers = dict()
-mus = dict()
-mus["###/0"] = math.log(1)
-
 
 if len(sys.argv) < 2:
     print "PUT YO FILES IN HERE YO"
@@ -163,4 +252,12 @@ test_file = sys.argv[2]
 
 train(training_file)
 test(test_file)
+forward_backward(test_file)
 
+
+
+
+
+    
+    
+    
